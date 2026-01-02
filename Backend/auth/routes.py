e651aba4219eb67e84993ef2f6e98b63d37a2e1a
Durairaj <ad@hpe.com>
2026-01-02 15:45:54 +0530

    Changed key name to message

--- Backend/auth/routes.py
+++ Backend/auth/routes.py
@@ -11,14 +11,14 @@ def register():
     data = request.get_json()
 
     if User.query.filter_by(email=data.get("email")).first():
-        return jsonify({"msg": "Email already registered"}), 400
+        return jsonify({"message": "Email already registered"}), 400
 
     user = User(email=data.get("email"),name=data.get("name"))
     user.set_password(data.get("password"))
 
     db.session.add(user)
     db.session.commit()
-    return jsonify({"msg": "User created successfully"}), 201
+    return jsonify({"message": "User created successfully"}), 201
 
 
 # Login route
@@ -28,10 +28,10 @@ def login():
     user = User.query.filter_by(email=data.get("email")).first()
 
     if not user or not user.check_password(data.get("password")):
-        return jsonify({"msg": "Invalid email or password"}), 401
+        return jsonify({"message": "Invalid email or password"}), 401
 
     access_token = create_access_token(identity=user.id)
-    return jsonify({"msg": "Logged in successfully", "token": access_token}), 200
+    return jsonify({"message": "Logged in successfully", "token": access_token}), 200
 
 @auth_bp.route("/health", methods=["GET"])
 def health_check():
